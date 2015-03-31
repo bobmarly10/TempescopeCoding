f917606e780106a7700ea7c01842ea1dd17f3d91
Campbell/Taylor <dorkrepublicgamediv@gmail.com>
2015-03-30 19:17:15 -0700

    Update index.php weather location

--- Website/index.php
+++ Website/index.php
@@ -1,5 +1,5 @@
 <?php
-$city="Eureka";
+$city="Arcata";
 $country="US"; //Two digit country code
 $url="http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&units=metric&cnt=7&lang=en";
 $json=file_get_contents($url);
