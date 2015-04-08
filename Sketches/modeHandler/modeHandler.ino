7cac4720a6e4f26652cd0d36bb49d481e7b3b916
Croberson12 <sdrawkcabisbackwardsbackwards@Gmail.com>
2015-04-07 18:50:37 -0700

    ooh baby baby, b-b-baby baby!

--- Sketches/modeHandler/modeHandler.ino
+++ Sketches/modeHandler/modeHandler.ino
@@ -3,11 +3,13 @@
 
 int mode = 0;
 
-byte server[] = { 10,0,0,8 };
+byte server[] = { 
+  67,188,251,155 };
 
-String location = "./ HTTP/1.0";
+String location = "/weathergetter/ HTTP/1.0";
 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte mac[] = { 
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 EthernetClient client;
 
@@ -22,21 +24,25 @@ void setup(){
 
 void loop(){
   String pageValue = connectAndRead();
-  
+
   if (pageValue == "01d" || pageValue == "01n" || pageValue == "02d" || pageValue == "02n") {
     mode = 0;
-  } else if (pageValue == "03d" || pageValue == "03n" || pageValue == "04d" || pageValue == "04n" || pageValue == "13d" || pageValue == "13n") {
+  } 
+  else if (pageValue == "03d" || pageValue == "03n" || pageValue == "04d" || pageValue == "04n" || pageValue == "13d" || pageValue == "13n") {
     mode = 1;
-  } else if (pageValue == "09d" || pageValue == "09n" || pageValue == "10d" || pageValue == "10n") {
+  } 
+  else if (pageValue == "09d" || pageValue == "09n" || pageValue == "10d" || pageValue == "10n") {
     mode = 2;
-  } else if (pageValue == "11d" || pageValue == "11n") {
+  } 
+  else if (pageValue == "11d" || pageValue == "11n") {
     mode = 3;
-  } else if (pageValue == "50d" || pageValue == "50n") {
+  } 
+  else if (pageValue == "50d" || pageValue == "50n") {
     mode = 4;
   }
-  
 
   Serial.println(pageValue);
+  Serial.println(mode);
 
   delay(30000); // Five minutes
 }
@@ -44,7 +50,7 @@ void loop(){
 String connectAndRead(){
 
   Serial.println("connecting...");
-  
+
   if (client.connect(server, 80)) {
     Serial.println("connected");
     client.print("GET ");
@@ -53,14 +59,15 @@ String connectAndRead(){
 
     return readPage();
 
-  }else{
+  }
+  else{
     return "connection failed";
   }
 
 }
 
 String readPage(){
-  
+
   stringPos = 0;
   memset( &inString, 0, 32 );
 
@@ -71,12 +78,14 @@ String readPage(){
 
       if (c == '<' ) {
         startRead = true;
-      }else if(startRead){
+      }
+      else if(startRead){
 
         if(c != '>'){
           inString[stringPos] = c;
           stringPos ++;
-        }else{
+        }
+        else{
           startRead = false;
           client.stop();
           client.flush();
@@ -91,3 +100,4 @@ String readPage(){
   }
 
 }
+
